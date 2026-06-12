e4b3508a178fcd4e23ac4382ed0393644351f3fb
aqeeeuu <ekaterinakleverova@gmail.com>
2026-06-13 00:44:10 +0300

    Add files via upload
    удалить сервер.го

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+ 	"log"
+ 	"net/http"
+)
+
+func main() {
+ 	// Главная страница (index.html)
+ 	http.Handle("/", http.FileServer(http.Dir("./")))
+
+ 	// Папка с товарами (поддерживает русские имена)
+ 	http.Handle(
+		"/products/",
+  		http.StripPrefix(
+			"/products/",
+   			http.FileServer(http.Dir("./products")),
+  		),
+ 	)
+
+ 	log.Println("Сервер запущен: http://localhost:8080")
+
+	err := http.ListenAndServe(":8080", nil)
+ 	if err != nil {
+  		log.Fatal(err)
+ 	}
+}
